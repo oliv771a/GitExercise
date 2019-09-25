d61b1f0cb9c772d3547c4ddedc141522bc906d58
Okaminu <4511415-Okaminu@users.noreply.gitlab.com>
2019-09-25 13:57:03 +0200

    Added the declariation to P3.34

--- src/P3_34.java
+++ src/P3_34.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class P3_34
 {
     public static void main(String[] args)
     {
-        // Assignment P3.34 
+        // Assignment P3.34
+        // Initialization
+        Scanner input = new Scanner(System.in);
+        System.out.println("Coupon calculator");
+
+        // Input
+        System.out.println("Please enter the total amount you spent.");
+        double totalAmount = input.nextDouble();
+
+        // The discounts calculations
+        double eightPercent = totalAmount * 0.08;
+        double tenPercent = totalAmount * 0.1;
+        double twelvePercent = totalAmount * 0.12;
+        double fourteenPercent = totalAmount * 0.14;
+
+        // TODO Need to make the if statements and the outputs
+
+
+
     }
 }
