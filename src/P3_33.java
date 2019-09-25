22c3c00c0a4dc5dd7386c3cd63613ede6b80c370
Islendingurinn <arnibent1312@hotmail.com>
2019-09-25 14:46:30 +0200

    Adding a test method

--- src/P3_33.java
+++ src/P3_33.java
@@ -1,13 +1,43 @@
+import java.util.Scanner;
+
 public class P3_33 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        double price = 0, finPrice = 0;
+        Scanner scanner = new Scanner(System.in);
+        double price = 0, tip = 0;
         int satisfaction = 0;
 
         // TODO We have to work on the rest of the project
 
-        
-    }
+        System.out.println("Please enter the total meal cost:");
+        if (scanner.hasNextDouble()) price = scanner.nextDouble();
+        else System.out.println("Please enter a valid number, in the form x.x");
 
+        System.out.println("How satisfied are you with the service?");
+        System.out.println("From 1 to 3, 1 being satisfied while 3 is dissatisfied.");
+
+        while (scanner.hasNextInt()) {
+
+            satisfaction = scanner.nextInt();
+            if (satisfaction <= 3 && satisfaction >= 1) break;
+            else System.out.println("Please enter in a number between 1 and 3.");
+
+        }
+
+        switch(satisfaction) {
+            case 1:
+                tip = price * 0.2;
+                break;
+            case 2:
+                tip = price * 0.15;
+                break;
+            case 3:
+                tip = price * 0.1;
+                break;
+        }
+
+        System.out.println("The total tip to be given is $" + tip);
+        System.out.println("The value was based on the satisfactory level " + satisfaction);
+    }
 }
